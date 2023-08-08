983212101df7da70b0cf8a8bc33814fb90b60f09
vrushabh.chandiwal <vrushabh.chandiwal@yash.com>
2023-08-08 11:24:19 +0000

    New Update from vrushabh

--- cube.js
+++ cube.js
@@ -5,4 +5,33 @@
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.
 
-module.exports = {};
+module.exports = {
+  // Expected security contexts
+  // https://cube.dev/docs/reference/configuration/config#scheduledrefreshcontexts
+  scheduledRefreshContexts: () => ([
+    { securityContext: { team: 'cx' } },
+    { securityContext: { team: 'executive' } },
+    { securityContext: { team: 'finance' } },
+    { securityContext: { team: 'marketing' } }
+  ]),
+
+  // Mapping of security contexts to data model versions
+  // https://cube.dev/docs/reference/configuration/config#contexttoappid
+  contextToAppId: ({ securityContext }) => {
+    return securityContext.team
+  },
+
+  // Security hook which is run before a query is executed
+  // https://cube.dev/docs/reference/configuration/config#queryrewrite
+  queryRewrite: (query, { securityContext }) => {
+    if (!securityContext.team) {
+      throw 'No team provided'
+    }
+
+    if (!['cx', 'executive', 'finance', 'marketing'].includes(securityContext.team)) {
+      throw 'Unknown team: ' + securityContext.team
+    }
+
+    return query
+  }
+}
